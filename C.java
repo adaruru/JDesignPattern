9513a973da41a990a960d7eef035584b81cdb860
Amanda <amanda.chou@frog-jump.com>
2020-09-28 21:02:08 +0800

    此時 編譯  javac -d . *.java  執行 java my.util.C import可以省略

---
+++ C.java
@@ -0,0 +1,11 @@
+package my.util;
+
+public class C {
+	public static void main(String[] args) {
+		System.out.println("Hello C!");
+		A a = new A();
+		B b = new B();
+		a.methodA();
+		b.methodB();
+	}
+}
