c0dc70d312a4dbdcc3f23f7256e310690a6b916e
Amanda <amanda.chou@frog-jump.com>
2020-10-05 21:29:52 +0800

    project 在C:\eclipse-workspace

---
+++ JDproject/src/Lab01.java
@@ -0,0 +1,26 @@
+import java.math.BigDecimal;
+
+public class Lab01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 123;
+		short b =1;
+		int ans1 = a-b;
+		double c=1.0000005;//§óºë·Ç
+		float d =1.0000005F;
+		double e =(1.3)*(2.2);
+		BigDecimal f =new BigDecimal("1.234");
+		
+		
+		System.out.println(a);
+		System.out.println(ans1);
+		System.out.println("-----------------");
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		
+	}
+
+}
---
+++ JDproject/src/Lab02.java
@@ -0,0 +1,9 @@
+
+public class Lab02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
